0d0a33e36281e87c2700efc58544a2b5655f0635
Jay <depixellife@gmail.com>
2021-03-31 07:01:58 +0100

    Registered settings

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,74 @@ import {
 
 joplin.plugins.register({
   onStart: async function () {
+    await joplin.settings.registerSection('myCustomSection', {
+      label: 'Meta',
+      iconName: 'fas fa-info',
+    });
+
+    await joplin.settings.registerSetting('myCustomSetting', {
+      value: 100,
+      type: SettingItemType.Int,
+      section: 'myCustomSection',
+      public: true,
+      label: 'Some random number',
+    });
+
+    await joplin.settings.registerSetting('another', {
+      value: true,
+      type: SettingItemType.Bool,
+      section: 'myCustomSection',
+      public: true,
+      label: 'Get Line number',
+    });
+
+    await joplin.settings.registerSetting('multiOptionTest', {
+      value: 'en',
+      type: SettingItemType.String,
+      section: 'myCustomSection',
+      isEnum: true,
+      public: true,
+      label: 'CHoose',
+      options: {
+        en: 'English',
+        fr: 'French',
+        es: 'Spanish',
+      },
+    });
+
+    await joplin.commands.register({
+      name: 'incValue',
+      label: 'Increment custom setting value',
+
+      iconName: 'fas fa-music',
+      execute: async () => {
+        const value = await joplin.settings.value('myCustomSetting');
+        console.info('Got value', value);
+        await joplin.settings.setValue('myCustomSetting', value + 1);
+      },
+    });
+
+    await joplin.commands.register({
+      name: 'checkValue',
+      label: 'Check custom setting value',
+      iconName: 'fas fa-drum',
+      execute: async () => {
+        const value = await joplin.settings.value('myCustomSetting');
+        alert('Current value is: ' + value);
+      },
+    });
+
+    await joplin.views.toolbarButtons.create(
+      'incValueButton',
+      'incValue',
+      ToolbarButtonLocation.NoteToolbar
+    );
+    await joplin.views.toolbarButtons.create(
+      'checkValueButton',
+      'checkValue',
+      ToolbarButtonLocation.NoteToolbar
+    );
+
     const dialog = joplin.views.dialogs;
 
     const handle = await dialog.create('myDialog1');
