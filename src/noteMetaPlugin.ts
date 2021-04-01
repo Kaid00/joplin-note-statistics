a9dbed3e83b23d524caecf6624d4ad787b2e18bb
Jay <depixellife@gmail.com>
2021-04-01 04:57:53 +0100

    determined note size units

--- src/noteMetaPlugin.ts
+++ src/noteMetaPlugin.ts
@@ -1,25 +1,62 @@
-export { countNum, countWords, countLines, countChar, countLetter };
+const markdownLinkExtractor = require('markdown-link-extractor');
+
+export {
+  countNum,
+  countWords,
+  countLines,
+  countChar,
+  countLetter,
+  getByteSize,
+};
+
+export function linksNumber(str) {
+  const linkNo = markdownLinkExtractor(str).length;
+  return linkNo;
+}
 
 function countWords(str) {
-  str = str.replace(/(^\s*)|(\s*$)/gi, "");
-  str = str.replace(/[ ]{2,}/gi, " ");
-  str = str.replace(/\n /, "\n");
-  return str.split(" ").length;
+  str = str.replace(/(^\s*)|(\s*$)/gi, '');
+  str = str.replace(/[ ]{2,}/gi, ' ');
+  str = str.replace(/\n /, '\n');
+  return str.split(' ').length;
 }
 
 function countLines(str) {
-  return str.split("\n").length;
+  return str.split('\n').length;
 }
 
 function countChar(str) {
-  str = str.replace(/\n /, "");
+  str = str.replace(/\n /, '');
   return str.length;
 }
 
 function countNum(str) {
-  return str.replace(/[^0-9]/g, "").length;
+  return str.replace(/[^0-9]/g, '').length;
 }
 
 function countLetter(str) {
-  return str.replace(/[^a-z]/gi, "").length;
+  return str.replace(/[^a-z]/gi, '').length;
+}
+
+// Claculates the size in bytes of the note
+function getByteSize(str) {
+  const byteSize = (str) => new Blob([str]).size;
+  const result = byteSize(str);
+
+  const convertBytes = function (result) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+    if (result == 0) {
+      return 'n/a';
+    }
+    const i = Math.floor(Math.log(result) / Math.log(1024));
+
+    if (i == 0) {
+      return result + ' ' + sizes[i];
+    }
+
+    return (result / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+  };
+
+  return convertBytes(result);
 }
